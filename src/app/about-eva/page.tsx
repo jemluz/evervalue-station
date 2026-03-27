1ab598d759305ad1f90c60e5fb79966c73e5bcf8
Jemima Luz <23065460+jemluz@users.noreply.github.com>
2026-03-27 19:04:27 -0300

    feat: add pages `/about-eva`, `/about-us` and `/conversor`
    - These pages serve as initial content stubs to be fleshed out with real data and functionality.

---
+++ src/app/about-eva/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function AboutEVAPage() {
+  return (
+    <div className="min-h-screen bg-white p-8 font-mono dark:bg-black dark:text-white">
+      <span className="text-sm text-red-400">
+        [this site its not a oficial EVA product, just a fun project to explore
+        the ecosystem and learn more about it]
+      </span>
+
+      <h1 className="mb-2 text-2xl font-bold">About EVA</h1>
+      <p className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+        Learn more about EVA and its features.
+      </p>
+
+      <ul className="space-y-2"></ul>
+    </div>
+  );
+}
